c66c71df11308da8d51ab40050f91a5d8b53be19
melfredroble <melfred.roble@evsu.edu.ph>
2023-03-21 22:40:45 +0800

    update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '@/styles/Signup.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
